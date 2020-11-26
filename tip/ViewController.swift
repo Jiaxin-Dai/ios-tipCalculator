e4cf69910f7f622a6ea59e31ab0fe917677d26c0
jiaxin <jdai13@hawk.iit.edu>
2020-11-25 19:19:23 -0600

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Jiaxin Dai on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
